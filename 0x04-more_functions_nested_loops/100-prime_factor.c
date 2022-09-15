559d379141b7a4774355439021f05f2db240c7fa
Aginam Abraham <Aabheaven@gmail.com>
2022-09-15 09:52:06 -0700

    d

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - prints the largest prime factor of 612852475143
+
+ *
+
+ * Return: 0
+
+ */
+
+
+
+int main(void)
+
+{
+
+	unsigned long int i, n;
+
+
+
+	n = 612852475143;
+
+	for (i = 3; i < 782849; i = i + 2)
+
+	{
+
+		while ((n % i == 0) && (n != i))
+
+		{
+
+			n = n / i;
+
+		}
+
+	}
+
+	printf("%lu\n", n);
+
+	return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+
+
+/**
+
+ * print_number - print integers
+
+ * @n: integer to print
+
+ * Return: void
+
+ */
+
+
+
+void print_number(int n)
+
+{
+
+	unsigned int num;
+
+
+
+	if (n < 0)
+
+	{
+
+		num = -n;
+
+		_putchar('-');
+
+	}
+
+	else
+
+	{
+
+		num = n;
+
+	}
+
+	if (num / 10)
+
+	{
+
+		print_number(num / 10);
+
+	}
+
+
+
+	_putchar(num % 10 + '0');
+
+}
