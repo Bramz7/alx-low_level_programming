390603138748435fecb5d8499b934d32ab4209a5
Aginam Abraham <Aabheaven@gmail.com>
2022-09-15 09:49:04 -0700

    s

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+
+ * main - main function
+
+ * Return: 0
+
+ */
+
+int main(void)
+
+{
+
+	int h = 1;
+
+
+
+	while (h <= 100)
+
+	{
+
+		if (h % 3 == 0 && h % 5 == 0)
+
+			printf("FizzBuzz ");
+
+		else if (h % 5 == 0)
+
+		{
+
+			if (h == 100)
+
+			{
+
+				printf("Buzz");
+
+				printf("\n");
+
+			}
+
+			else
+
+				printf("Buzz ");
+
+		}
+
+		else if (h % 3 == 0)
+
+			printf("Fizz ");
+
+		else
+
+			printf("%d ", h);
+
+	h++;
+
+	}
+
+	return (0);
+
+}
